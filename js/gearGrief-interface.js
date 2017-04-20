735ca9c175324b3055b7eab2f59d003ffcce78b4
liam stabeno and riley watts <liam@>
2017-04-20 15:21:55 -0700

    add googlemaps api, key needed

--- js/gearGrief-interface.js
+++ js/gearGrief-interface.js
@@ -1,15 +1,19 @@
 var BikeData = require('./../js/gearGrief.js').thingy;
-
+var currentBike = [];
+console.log("currentBikeArrray1" , currentBike);
 
 $(function(){
+
+
   var newBike = new BikeData();
   $("#bike-form").submit(function(event){
     event.preventDefault();
     var type = $("#bikeType").val();
     var location = $("#bikeLocation").val();
     console.log(type,location);
-    newBike.getData(type, location);
+    currentBike.push(newBike.getData(type, location));
+    console.log("currentBikeArrray" , currentBike);
   });
 
-  console.log(newBike);
+
 });
--- js/gearGrief.js
+++ js/gearGrief.js
@@ -1,11 +1,16 @@
 
+var currentBike = [];
 function BikeData() {
 
 }
   BikeData.prototype.getData = function(type, location) {
     $.get('https://bikeindex.org:443/api/v3/search?page=2&per_page=25&manufacturer=' + type + '&location=' + location + '&distance=1&stolenness=proximity')
     .then(function(response) {
-      console.log(response);
+       for (var i = 0; i < response.bikes.length; i++) {
+        $("#bikes").append("<li>"+response.bikes[i].stolen_location+"</li>");
+        console.log(response.bikes[i].stolen_location);
+        console.log(i);
+      }
     });
   };
 
